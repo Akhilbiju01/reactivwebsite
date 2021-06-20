de30f53cc877885e28dba1da5b9bfef46275eb9c
AKHIL BIJU <akhilbiju64@.com>
2021-06-20 10:04:51 +0530

    Updates

---
+++ precache-manifest.04d0282cad7ccfd354ac916578d141a1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c776cca7ca57c7a9dd0403d05c737e5b",
+    "url": "/reactivwebsite/index.html"
+  },
+  {
+    "revision": "79f584faef92e3e857d7",
+    "url": "/reactivwebsite/static/css/main.cb5eefb9.chunk.css"
+  },
+  {
+    "revision": "1f756e7828f95d4c4a69",
+    "url": "/reactivwebsite/static/js/2.32a66302.chunk.js"
+  },
+  {
+    "revision": "79f584faef92e3e857d7",
+    "url": "/reactivwebsite/static/js/main.1212c520.chunk.js"
+  },
+  {
+    "revision": "6b011a55fa9915969cf7",
+    "url": "/reactivwebsite/static/js/runtime~main.5da7e753.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/reactivwebsite/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/reactivwebsite/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/reactivwebsite/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactivwebsite/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/reactivwebsite/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/reactivwebsite/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/reactivwebsite/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/reactivwebsite/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/reactivwebsite/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "1f09edbc706352e8dc43f0f2cd44b333",
+    "url": "/reactivwebsite/static/media/blog-blue-network.1f09edbc.png"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/reactivwebsite/static/media/roboto-regular.f36638c2.ttf"
+  }
+]);
